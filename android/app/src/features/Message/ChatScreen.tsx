c137b9c35286c1f9405e0a6cb48d164e7bb8d090
Alice Goarnisson <a_goarnisson@hetic.eu>
2024-10-14 08:16:16 +0000

    Revert Dev build

--- android/app/src/features/Message/ChatScreen.tsx
+++ android/app/src/features/Message/ChatScreen.tsx
@@ -1,12 +1,11 @@
 // ChatScreen.tsx
-import React, { useEffect, useState, useRef, useContext } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 import { getSocket } from '../../socket';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import { color, spacing } from 'src/styles'; // Import des styles
-import { AuthContext } from 'src/contexts/AuthContext';
 
 interface Message {
   id: number;
@@ -26,23 +25,27 @@ const ChatScreen = ({ route }: any) => {
   const receiverId = Number(receiverIdParam);
   const [messages, setMessages] = useState<Message[]>([]);
   const [inputMessage, setInputMessage] = useState('');
-  const { state } = useContext(AuthContext); // Utilisation de state et logout à partir du contexte
-  const token = state.token;
-  const userId = state.userId;
-  
+  const [userId, setUserId] = useState<number | null>(null);
+
   const flatListRef = useRef<FlatList>(null);
 
   useEffect(() => {
     const initializeChat = async () => {
       // Récupérer l'ID de l'utilisateur à partir du token
+      const token = await AsyncStorage.getItem('token');
       if (token) {
+        const decoded: DecodedToken = jwtDecode(token);
+        console.log('Decoded token:', decoded);
+        const currentUserId = decoded.id;
+        setUserId(currentUserId);
+
         // Récupérer les messages
-        await fetchMessages(userId);
+        await fetchMessages(currentUserId);
       }
     };
 
     initializeChat();
-  }, [userId]);
+  }, []);
 
   useEffect(() => {
     if (userId !== null) {
@@ -73,12 +76,12 @@ const ChatScreen = ({ route }: any) => {
     }
   }, [messages]);
 
-  const fetchMessages = async (userId: number | null) => {
-    console.log('Fetching messages between', userId, 'and', receiverId);
+  const fetchMessages = async (currentUserId: number) => {
+    console.log('Fetching messages between', currentUserId, 'and', receiverId);
     try {
       const token = await AsyncStorage.getItem('token');
       const response = await axios.get(
-        `https://n0kia-0024.com/messages/${userId}/${receiverId}`,
+        `https://n0kia-0024.com/messages/${currentUserId}/${receiverId}`,
         {
           headers: {
             Authorization: token,
