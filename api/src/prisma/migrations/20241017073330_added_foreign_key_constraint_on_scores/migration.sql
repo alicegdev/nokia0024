c73e4cb54ef94f1887ec7bcccf633a1ca9f6450c
Alice Goarnisson <a_goarnisson@hetic.eu>
2024-10-17 11:13:59 +0000

    Revert "Merge branch 'fix/high-scores-queries' into 'main'"
    This reverts merge request !75

--- api/src/prisma/migrations/20241017073330_added_foreign_key_constraint_on_scores/migration.sql
+++
@@ -1,7 +0,0 @@
--- This is an empty migration.
-
-ALTER TABLE "Score" RENAME COLUMN "Game" TO "game";
-ALTER TABLE "Score" RENAME COLUMN "User" TO "user";
-
-ALTER TABLE "Score" ADD CONSTRAINT "fk_gameId" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE CASCADE;
-ALTER TABLE "Score" ADD CONSTRAINT "fk_userId" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE;
